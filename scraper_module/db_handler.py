7b8d5350cbae6b45026a5ae0f1798b83b68449ca
Paul Easter <paul.john.easter@gmail.com>
2023-11-13 17:19:25 +1100

    db_handler modified to SQL Server

--- scraper_module/db_handler.py
+++ scraper_module/db_handler.py
@@ -1,61 +1,90 @@
 """
 db_handler.py
 
-This module provides a generic interface for interacting with a PostgreSQL database. 
-It includes functionalities to connect to the database, execute SQL queries, fetch results, 
+This module provides a generic interface for interacting with a SQL Server database. 
+It supports both Windows Authentication and SQL Server Authentication. The module 
+includes functionality to connect to the database, execute SQL queries, fetch results, 
 and close the connection.
 
+The module uses an enum `AuthMethod` to specify the authentication method.
+
 Example:
-    from db_handler import DBHandler
-    db = DBHandler(dbname="sample_db", user="user", password="password")
+    from db_handler import DBHandler, AuthMethod
+
+    # For Windows Authentication
+    db = DBHandler(dbname="sample_db", auth_method=AuthMethod.WINDOWS_AUTH)
+    db.connect()
+    results = db.fetch("SELECT * FROM sample_table")
+    db.close()
+
+    # For SQL Server Authentication
+    db = DBHandler(dbname="sample_db", auth_method=AuthMethod.SQL_SERVER_AUTH, user="user", password="password")
     db.connect()
     results = db.fetch("SELECT * FROM sample_table")
     db.close()
 """
 
-import psycopg2
+from enum import Enum, auto
+import pyodbc
+
+class AuthMethod(Enum):
+    WINDOWS_AUTH = auto()
+    SQL_SERVER_AUTH = auto()
 
 class DBHandler:
     """
-    A handler for database operations on a PostgreSQL database.
+    A handler for database operations on a SQL Server database.
     
-    This class provides methods to connect to a PostgreSQL database, execute SQL commands,
-    fetch data from the database, and close the connection. It's designed to be a flexible and
-    reusable component for database operations.
-    
+    This class provides methods to connect to a SQL Server database, execute SQL commands,
+    fetch data from the database, and close the connection. It supports both Windows 
+    Authentication and SQL Server Authentication, determined by the `auth_method` parameter.
+
     Attributes:
         dbname (str): Name of the database.
-        user (str): Database user name.
-        password (str): Password for the database user.
+        auth_method (AuthMethod): The method of authentication to use (Windows or SQL Server).
+        user (str, optional): Database user name. Required if using SQL Server Authentication.
+        password (str, optional): Password for the database user. Required if using SQL Server Authentication.
         host (str, optional): Host of the database. Defaults to "localhost".
-        port (str, optional): Port to connect on. Defaults to "5432".
-        conn (psycopg2.extensions.connection): The database connection object.
-    
-    Example:
-        db_handler = DBHandler(dbname="mydb", user="user", password="pass")
+        port (str, optional): Port to connect on. Defaults to "1433".
+        conn (pyodbc.Connection): The database connection object.
+
+    Example for Windows Authentication:
+        db_handler = DBHandler(dbname="mydb", auth_method=AuthMethod.WINDOWS_AUTH)
         db_handler.connect()
-        db_handler.execute("INSERT INTO table_name (column1, column2) VALUES (value1, value2)")
+        results = db_handler.fetch("SELECT * FROM table_name")
+        db_handler.close()
+
+    Example for SQL Server Authentication:
+        db_handler = DBHandler(dbname="mydb", auth_method=AuthMethod.SQL_SERVER_AUTH, user="user", password="pass")
+        db_handler.connect()
         results = db_handler.fetch("SELECT * FROM table_name")
         db_handler.close()
     """
+
     # pylint: disable=too-many-arguments
-    def __init__(self, dbname, user, password, host="localhost", port="5432"):
+    def __init__(self, dbname, auth_method=AuthMethod.WINDOWS_AUTH, host="localhost", port="1433", user=None, password=None):
         self.dbname = dbname
-        self.user = user
-        self.password = password
         self.host = host
         self.port = port
+        self.user = user
+        self.password = password
+        self.auth_method = auth_method
         self.conn = None
 
     def connect(self):
         """Establish a connection to the database."""
-        self.conn = psycopg2.connect(
-            dbname=self.dbname,
-            user=self.user,
-            password=self.password,
-            host=self.host,
-            port=self.port
-        )
+        if self.auth_method == AuthMethod.SQL_SERVER_AUTH and self.user and self.password:
+            # SQL Server Authentication
+            self.conn = pyodbc.connect(
+                f'DRIVER={{SQL Server}};SERVER={self.host};DATABASE={self.dbname};UID={self.user};PWD={self.password}'
+            )
+        elif self.auth_method == AuthMethod.WINDOWS_AUTH:
+            # Windows Authentication
+            self.conn = pyodbc.connect(
+                f'DRIVER={{SQL Server}};SERVER={self.host};DATABASE={self.dbname};Trusted_Connection=yes;'
+            )
+        else:
+            raise ValueError("Invalid authentication method or credentials")
         return self.conn
 
     def execute(self, query, params=None):
